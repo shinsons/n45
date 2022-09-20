0c1721735c367edaf7900e109caf6f06d3d97395
Nathen Hinson <nathen.hinson@gmail.com>
2022-09-20 19:50:20 +0000

    MVP of N45 app/timer and initial week of workouts.

---
+++ assets/js/index.js
@@ -0,0 +1,112 @@
+var app = document.querySelector('div.app');
+var summaryEl = document.querySelector('div.summary');
+var timerEl = document.querySelector('div.timer-container');
+var descriptionEl = document.querySelector('div.description');
+var listEl = document.querySelector('ul');
+var timeEl = document.querySelector('h1');
+var nextEl = document.querySelector('div.next');
+var doneEl = document.querySelector('h3.done');
+var whistle = new Howl({
+  src: ['/assets/sound/start.wav']
+});
+var rest = new Howl({
+  src: ['/assets/sound/finish.wav']
+});
+
+class WOD { 
+
+  constructor(db_entry) {
+    this.wod = db_entry;
+    this.currentStation = undefined;
+    this.lastTick = undefined;
+  }
+
+  list() {
+    const listContent = [];
+    this.wod.workout.forEach((name) => { 
+      listContent.push('<li>' + name + '</li>') 
+    }); 
+    listEl.innerHTML = listContent.join('\n');
+    descriptionEl.innerHTML = this.wod.description;
+    addRemoveClass([summaryEl], 'hide', 'remove');
+    addRemoveClass([summaryEl], 'show');
+  }
+
+  start() {
+    this.currentStation = this.wod.stations.shift();
+    whistle.play();
+    addRemoveClass([app], 'blue', 'remove');
+    addRemoveClass([app], 'green');
+    addRemoveClass([summaryEl], 'show', 'remove');
+    addRemoveClass([summaryEl], 'hide');
+    nextEl.innerHTML = this.currentStation.name;
+    addRemoveClass([timerEl, nextEl], 'hide', 'remove');
+    addRemoveClass([timerEl, nextEl], 'show');
+    requestAnimationFrame(this.update.bind(this));
+  }
+
+  update(timestamp) {
+    if(!this.lastTick || timestamp - this.lastTick >= 1000) {
+      if(this.currentStation.work <= 0 && this.currentStation.rest <= 0) {
+        if(this.wod.stations.length === 0) {
+          this.done();
+          return;
+        }
+        addRemoveClass([app], 'red', 'remove');
+        addRemoveClass([app], 'green');
+        whistle.play();
+        this.currentStation = this.wod.stations.shift();
+        nextEl.innerHTML = this.currentStation.name;
+        timeEl.innerHTML = this.currentStation.work;
+        this.currentStation.work = this.currentStation.work -1;
+      }
+      else if(this.currentStation.work > 0) {
+        timeEl.innerHTML = this.currentStation.work;
+        this.currentStation.work = this.currentStation.work -1;
+      }
+      else if(this.currentStation.rest > 0) {
+        if(this.wod.stations.length > 0) {
+          nextEl.innerHTML = 'Next Up: ' + this.wod.stations[0].name;
+        }
+        addRemoveClass([app], 'green', 'remove');
+        addRemoveClass([app], 'red');
+        if(this.currentStation.rest === 2) {
+          rest.play();
+        }
+        timeEl.innerHTML = this.currentStation.rest;
+        this.currentStation.rest = this.currentStation.rest -1;
+      }
+      this.lastTick = timestamp;
+    }
+    requestAnimationFrame(this.update.bind(this));
+  }
+  
+  done() {
+    addRemoveClass([app], 'red', 'remove');
+    addRemoveClass([app], 'green');
+    addRemoveClass([timerEl, nextEl], 'show', 'remove');
+    addRemoveClass([timerEl, nextEl], 'hide');
+    addRemoveClass([doneEl], 'hide', 'remove');
+    addRemoveClass([doneEl], 'show');
+  }
+};
+
+function addRemoveClass(els, modification, remove) {
+  for (var i = 0; i < els.length; i++) {
+    if(remove) {
+      els[i].classList.remove(modification);
+      continue
+    }
+    els[i].classList.add(modification);
+  }
+};
+
+function getWOD() {
+  const today = new Date();
+  const wod_key = today.getFullYear() +
+    '-' + new Number(today.getMonth() + 1) + 
+    '-' + today.getDate();
+  return db[wod_key]
+};
+var thisWod = new WOD(getWOD());
+thisWod.list();
